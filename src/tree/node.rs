9c39816f6f3c5cecee2f466e074f0e87e5b81e98
GitLab <gitlab@ebarreto>
2017-01-20 16:49:13 -0200

    Implementing Hash and Eq for Node struct

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,7 +1,12 @@
-use wrapper::pugi_delete_node;
+use libc::c_void;
+
+use std::cmp::Eq;
+
+use std::hash::{Hash, Hasher};
 
 use tree::NodeBase;
 
+use wrapper::{pugi_delete_node, pugi_node_equal};
 
 pub enum CNode {}
 
@@ -22,6 +27,19 @@ impl Drop for Node {
     }
 }
 
+impl Eq for Node {}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Node) -> bool {
+        unsafe { pugi_node_equal(self.ptr, other.ptr) == 1 }
+    }
+}
+
+impl Hash for Node {
+    fn hash<H>(&self, state: &mut H) where H: Hasher {
+        self.ptr.hash(state)
+    }
+}
 
 impl Node {
     pub fn from_ptr(ptr: *mut CNode) -> Result<Self, ()> {
