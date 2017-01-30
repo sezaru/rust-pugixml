7b4a4546443f5e8ebc3155f227354b1c6b0f9506
GitLab <gitlab@ebarreto>
2017-01-30 10:36:51 -0200

    Adding sysroot temporary test

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main()
         .file("wrapper/wrapper.cpp")
         .file("pugixml/src/pugixml.cpp")
         .include("pugixml/src")
-        .flag("-std=c++11")
-        .compile("libpugixml.a")
+        .cpp_set_stdlib(Some("c++"))
+        .flag("--sysroot=/home/ebarreto/x-tools/i686-unknown-linux-gnu/i686-unknown-linux-gnu/sysroot")
+        .compile("libpugixml.a");
 }
